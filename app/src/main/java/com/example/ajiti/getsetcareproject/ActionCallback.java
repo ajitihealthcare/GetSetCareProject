6dcec16d904b0498b782874b013db15a85957f04
ajitihealthcare <getsethealthy@gmail.com>
2016-12-06 18:24:26 +0530

    j

---
+++ app/src/main/java/com/example/ajiti/getsetcareproject/ActionCallback.java
@@ -0,0 +1,12 @@
+package com.example.ajiti.getsetcareproject;
+
+import java.util.Map;
+
+/**
+ * Created by AJITI on 12/6/2016.
+ */
+
+public interface ActionCallback
+{
+    public void onCallback(Map<String, String> param);
+}
