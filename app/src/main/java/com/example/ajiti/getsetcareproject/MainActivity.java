10cfea3131f1609d332d6e45885fb10cbfb68c82
ajitihealthcare <getsethealthy@gmail.com>
2016-12-06 17:55:14 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/ajiti/getsetcareproject/MainActivity.java
+++ app/src/main/java/com/example/ajiti/getsetcareproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"getsetcare",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"getsetcaresolutions",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"getsetcaresolutionspvtLtd",Toast.LENGTH_SHORT).show();
 
     }
 }
