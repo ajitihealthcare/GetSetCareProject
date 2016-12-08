e488fcddf795366df17d0095c901da7f61ad6079
ajitihealthcare <getsethealthy@gmail.com>
2016-12-08 18:18:07 +0530

    Update ProviderSignUpActivity.java

--- app/src/main/java/com/example/ajiti/getsetcareproject/ProviderSignUpActivity.java
+++ app/src/main/java/com/example/ajiti/getsetcareproject/ProviderSignUpActivity.java
@@ -19,5 +19,6 @@ public class ProviderSignUpActivity extends AppCompatActivity{
           Toast.makeText(getApplicationContext(),"rekapalli babji",Toast.LENGTH_SHORT).show();
           Toast.makeText(getApplicationContext(),"getsetcare",Toast.LENGTH_SHORT).show();
           Toast.makeText(getApplicationContext(),"getsetcaresolutions",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"getsetcaresolutions1",Toast.LENGTH_SHORT).show();
       }
 }
