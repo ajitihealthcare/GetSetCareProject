560c0b1a4e42414ae54a11101eb49e0cf38e26db
ajitihealthcare <getsethealthy@gmail.com>
2016-12-09 19:14:53 +0530

    m

---
+++ app/src/main/java/com/example/ajiti/getsetcareproject/AccountRoomActivity.java
@@ -0,0 +1,10 @@
+package com.example.ajiti.getsetcareproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AJITI on 12/9/2016.
+ */
+
+public class AccountRoomActivity extends AppCompatActivity {
+}
