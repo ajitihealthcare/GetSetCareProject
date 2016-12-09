0928667e73702260c1b5bd7cd2f51a9c193e74e1
ajitihealthcare <getsethealthy@gmail.com>
2016-12-09 11:42:35 +0530

    m

---
+++ app/src/main/java/com/example/ajiti/getsetcare/ProviderDTO.java
@@ -0,0 +1,14 @@
+package com.example.ajiti.getsetcare;
+
+/**
+ * Created by AJITI on 12/9/2016.
+ */
+
+public class ProviderDTO {
+
+    private String name;
+    private String id;
+    private String dob;
+    private String address;
+    private String city;
+}
